3d7a758a1cbd2efbe8c455ec836d13ce5a3ba15a
Abdallah El-Zoka <abdallah.elzoka@gmail.com>
2025-01-15 19:33:15 +0200

    Initial Commit

---
+++ app/src/main/java/com/route/islamiappc41gsunwed/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.route.islamiappc41gsunwed
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // 1- Version Control Systems (VCS) (Git & Github)
+        // 2- Islami
+
+        //      Git ?
+        //  1- Client -> Application ->
+        // 1- Versioned Files
+        // 2- UnVersioned Files
+        // 3- Ignored Files
+    }
+}
