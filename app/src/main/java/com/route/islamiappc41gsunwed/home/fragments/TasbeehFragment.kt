2fc5aea578eea52acdaf1747b5e0f7cdfd3a7f75
ahmad <ahmad1zain10@gmail.com>
2025-02-08 16:12:56 +0200

    Assignment #5

--- app/src/main/java/com/route/islamiappc41gsunwed/home/fragments/TasbeehFragment.kt
+++ app/src/main/java/com/route/islamiappc41gsunwed/home/fragments/TasbeehFragment.kt
@@ -5,11 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.route.islamiappc41gsunwed.databinding.FragmentHadethBinding
-import com.route.islamiappc41gsunwed.databinding.FragmentQuranBinding
+import com.route.islamiappc41gsunwed.R
 import com.route.islamiappc41gsunwed.databinding.FragmentTasbeehBinding
 
 class TasbeehFragment : Fragment() {
+    private var azkarCounter = 0
+    private var azkarCounterPosition = 0
+    lateinit var azkList: MutableList<String>
     lateinit var binding: FragmentTasbeehBinding
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,6 +24,30 @@ class TasbeehFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        azkList = resources.getStringArray(R.array.azkarlist).toMutableList()
+        initViews()
+        sebhaclick()
+    }
 
+    private fun initViews() {
+        binding.txZek.text = azkList[azkarCounterPosition]
+        binding.count.text = azkarCounter.toString()
+    }
+
+    private fun sebhaclick() {
+        binding.imgSebha.setOnClickListener {
+            binding.imgSebha.rotation += (360 / 33).toFloat()
+            if (azkarCounter < 33) {
+                azkarCounter++
+                binding.count.text = azkarCounter.toString()
+            } else {
+                azkarCounter = 0
+                azkarCounterPosition =
+                    if (azkarCounterPosition < azkList.size - 1) azkarCounterPosition + 1 else 0
+                binding.txZek.text = azkList[azkarCounterPosition]
+            }
+            binding.count.text = azkarCounter.toString()
+        }
     }
 }
+
